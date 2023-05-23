a1168feb1127101b7f2114bbce98f8247b1b2e1c
Arguns <paraskyausa23@gmail.com>
2023-05-23 17:39:25 +0545

    Day 4

--- recipebook/vege/views.py
+++ recipebook/vege/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def recipes(request):
-    context= {'page':'Recipes'}
+    context = {'page':'Recipe'}
+    if request.method=="POST":
+        data = request.POST
+
+        recipe_image = request.FILES.get('recipe_image')
+        recipe_name = data.get('recipe_name')
+        recipe_description = data.get('recipe_description')
+
+        Recipe.objects.create(
+            recipe_image = recipe_image,
+            recipe_name = recipe_name,
+            recipe_description = recipe_description,
+        )
+
     return render(request, 'recipes.html', context)
