4bdc20d723dde9683a4e577d9b4dce7e987e7452
Arguns <paraskyausa23@gmail.com>
2023-05-27 09:41:07 +0545

    Adding Login

--- recipebook/vege/views.py
+++ recipebook/vege/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from .models import *
 
@@ -58,6 +59,24 @@ def update_recipe(request, id):
 
 
 def user_login(request):
+
+    if request.method == "POST":
+        
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if not User.objects.filter(username=username).exists():
+            messages.error(request,"invalid username")
+            return redirect('login/')
+        
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            messages.error(request,'Invalid Account')
+            return redirect('login/')
+        else:
+            login(request,user)
+            return redirect('recipes/')
 
     return render(request, 'login.html')
 
